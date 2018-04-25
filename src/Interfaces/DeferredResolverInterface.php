eb536e6490c938f3f43bac2fbc44b5689a3361bb
joskfg <joskfg@gmail.com>
2018-04-25 17:59:09 +0200

    Remove array type hinting in roots
    Somethimes we could receive objects with array interface, so we cannot force to be a basic array.

--- src/Interfaces/DeferredResolverInterface.php
+++ src/Interfaces/DeferredResolverInterface.php
@@ -14,14 +14,14 @@ interface DeferredResolverInterface
     /**
      * Process all roots and return all the information obtained.
      *
-     * @param array $roots
+     * @param mixed $roots
      * @param mixed $args 
      * @param mixed $context
      * @param mixed $info
      *
      * @return mixed
      */
-    public function fetch(array $roots, $args, $context, $info);
+    public function fetch($roots, $args, $context, $info);
 
     /**
      * Returns the data depending on the root.
