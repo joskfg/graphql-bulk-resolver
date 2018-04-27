afd6f23d074c83ba8ae447e5e42e0aa188b657eb
joskfg <joskfg@gmail.com>
2018-04-27 12:06:20 +0200

    Allow circle relationship

--- src/DeferredResolverTrait.php
+++ src/DeferredResolverTrait.php
@@ -7,21 +7,22 @@ use Joskfg\GraphQLBulk\Interfaces\DeferredResolverInterface;
 
 trait DeferredResolverTrait
 {
-    private static $buffer = [
-        'roots'   => [],
-        'results' => [],
-    ];
+    private static $buffer = [];
 
-    private function deferredResolver(DeferredResolverInterface $resolver):\Closure
+    private function deferredResolver(DeferredResolverInterface $resolver): \Closure
     {
-        return function ($root, array $args = [], $context = null, $info = null) use ($resolver):Deferred {
-            self::$buffer['roots'][] = $root;
+        return function ($root, array $args = [], $context = null, $info = null) use ($resolver): Deferred {
+            $resolverName = get_class($resolver);
+            self::$buffer[$resolverName] ?? self::$buffer[$resolverName] = [];
 
-            $buffer = &self::$buffer;
+            $buffer            = &self::$buffer[$resolverName];
+            $buffer['results'] = [];
+            $buffer['roots'][] = $root;
 
             return new Deferred(function () use (&$buffer, $resolver, $root, $args, $context, $info) {
                 if (empty($buffer['results'])) {
                     $buffer['results'] = $resolver->fetch($buffer['roots'], $args, $context, $info);
+                    $buffer['roots']   = [];
                 }
 
                 return $resolver->pluck($root, $buffer['results']);
--- tests/DeferredResolverTraitTest.php
+++ tests/DeferredResolverTraitTest.php
@@ -50,13 +50,13 @@ class DeferredResolverTraitTest extends TestCase
 
         $resolveField = $deferredResolver->run($resolver);
 
-        $firstPromise  = $resolveField('root1', ['args'], 'context', 'info');
+        $firstPromise = $resolveField('root1', ['args'], 'context', 'info');
         $secondPromise = $resolveField('root2', ['args'], 'context', 'info');
-        $thirdPromise  = $resolveField('root3', ['args'], 'context', 'info');
+        $thirdPromise = $resolveField('root3', ['args'], 'context', 'info');
 
-        $firstResult  = null;
+        $firstResult = null;
         $secondResult = null;
-        $thirdResult  = null;
+        $thirdResult = null;
 
         $firstPromise->then(function ($result) use (&$firstResult) {
             $firstResult = $result;
@@ -70,9 +70,68 @@ class DeferredResolverTraitTest extends TestCase
 
         $this->resolvePromises();
 
-        $this->assertEquals($firstResult, 'result1');
-        $this->assertEquals($secondResult, 'result2');
-        $this->assertEquals($thirdResult, 'result3');
+        $this->assertEquals('result1', $firstResult);
+        $this->assertEquals('result2', $secondResult);
+        $this->assertEquals('result3', $thirdResult);
+    }
+
+    /**
+     * @test
+     */
+    public function whenReceiveNewCallsAfterFetchItShouldReturnJustTheNewData()
+    {
+        $resolver = \Mockery::mock(DeferredResolverInterface::class);
+        $deferredResolver = $this->getResolver();
+
+        $resolver->shouldReceive('fetch')
+            ->with(
+                ['root1'],
+                ['args'],
+                'context',
+                'info'
+            )
+            ->andReturn(['result1']);
+        $resolver->shouldReceive('pluck')
+            ->with(
+                'root1',
+                ['result1']
+            )
+            ->andReturn('result1');
+
+        $resolver->shouldReceive('fetch')
+            ->with(
+                ['root2'],
+                ['args'],
+                'context',
+                'info'
+            )
+            ->andReturn(['result2']);
+        $resolver->shouldReceive('pluck')
+            ->with(
+                'root2',
+                ['result2']
+            )
+            ->andReturn('result2');
+
+        $resolveField = $deferredResolver->run($resolver);
+
+        $firstPromise  = $resolveField('root1', ['args'], 'context', 'info');
+        $firstResult   = null;
+        $firstPromise->then(function ($result) use (&$firstResult) {
+            $firstResult = $result;
+        });
+
+        $this->resolvePromises();
+        $this->assertEquals('result1', $firstResult);
+
+        $secondPromise = $resolveField('root2', ['args'], 'context', 'info');
+        $secondResult = null;
+        $secondPromise->then(function ($result) use (&$secondResult) {
+            $secondResult = $result;
+        });
+
+        $this->resolvePromises();
+        $this->assertEquals('result2', $secondResult);
     }
 
     private function getResolver()
